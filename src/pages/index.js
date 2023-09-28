727558967a6017da76a5c51c484f387e2b6603ff
oscarmangs <oscar.maangs@gmail.com>
2023-09-28 14:52:36 +0200

    added featured posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,51 @@
+import FeaturedPosts from "../../components/home-page/featured-posts";
 import Hero from "../../components/home-page/hero";
 
-function HomePage(){
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-next-js1",
+    title: "getting started with next js",
+    image: "getting-started-nextjs.png",
+    excerpt: "next js is a great framework for production",
+    date: "2023-09-10",
+  },
+  {
+    slug: "getting-started-with-next-js2",
+    title: "getting started with next js",
+    image: "getting-started-nextjs.png",
+    excerpt: "next js is a great framework for production",
+    date: "2023-09-10",
+  },
+  {
+    slug: "getting-started-with-next-js3",
+    title: "getting started with next js",
+    image: "getting-started-nextjs.png",
+    excerpt: "next js is a great framework for production",
+    date: "2023-09-10",
+  },
+  {
+    slug: "getting-started-with-next-js4",
+    title: "getting started with next js",
+    image: "getting-started-nextjs.png",
+    excerpt: "next js is a great framework for production",
+    date: "2023-09-10",
+  },
+  {
+    slug: "getting-started-with-next-js5",
+    title: "getting started with next js",
+    image: "getting-started-nextjs.png",
+    excerpt: "next js is a great framework for production",
+    date: "2023-09-10",
+  },
+];
+
+function HomePage() {
   return (
     <>
-    <Hero />
+      <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
-  )
+  );
 }
 
-export default HomePage; 
+export default HomePage;
